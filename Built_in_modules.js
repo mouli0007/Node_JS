598def3b6bcc9ea26e251fe94eb9e04fdf675031
Mouli VJ <75278840+mouli0007@users.noreply.github.com>
2023-04-14 00:38:20 +0530

    Update Built_in_modules.js

--- Built_in_modules.js
+++ Built_in_modules.js
@@ -22,3 +22,24 @@ const obj = {
 };
 
 console.log(obj);
+
+############## File-Path###################
+
+// Interacting with File path
+
+// Getting a seperate path of the current file
+const path = require("path");
+console.log(path.sep);
+
+// Getting a specific filepath
+const filePath = path.join("/content", "sub-folder", "test.txt");
+console.log(filePath);
+
+// Getting a specific file
+const base = path.basename(filePath);
+console.log(base);
+
+// Getting the whole system directory path with the file path
+const absolute = path.resolve(__dirname, "content", "sub-folder", "text.txt");
+console.log(absolute);
+console.log(__dirname);
