0f30a6438e74d054f6af1452278757449567db35
Mouli VJ <75278840+mouli0007@users.noreply.github.com>
2023-04-15 00:16:36 +0530

    Update EVENTS_Nodejs.js

--- EVENTS_Nodejs.js
+++ EVENTS_Nodejs.js
@@ -1,1 +1,26 @@
 
+const EventEmitter = require("events");
+
+const customEmitter = new EventEmitter();
+
+customEmitter.on("response", ({ age }) => {
+  console.log(`Data Recived and Event Emitted !`);
+  console.log(age);
+});
+
+customEmitter.on("response", (name) => {
+  console.log("Some other Logic here !");
+  console.log(name);
+});
+
+customEmitter.emit("response", { name: "Mouli VJ", age: 25 });
+
+const http = require("http");
+
+const server = http.createServer();
+
+server.on("request", (req, res) => {
+  res.end("Welcome to my home page !");
+});
+
+server.listen(4000);
